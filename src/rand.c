4146bfbed9aba4a5f2587a308ed00082af5ccd99
sbadoni <sbadoni@crestron.com>
2022-03-13 20:41:18 +0530

    random_int: use rescaling

--- src/rand.c
+++ src/rand.c
@@ -9,5 +9,5 @@ fprintf(stderr, "Usage: %s <number>\n", argv[0]); return EXIT_FAILURE;
 }
 int max = atoi(argv[1]);
 int result = random_int(max); printf("%d\n", result);
-     return EXIT_SUCCESS;  
+     return EXIT_SUCCESS;     
    }
